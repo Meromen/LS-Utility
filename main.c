d8273380fb366a56eb7a58f9a0c070af8fa18f8b
Meromen <32484121+Meromen@users.noreply.github.com>
2018-08-12 09:44:27 +1000

    CHANGE reading of data

--- main.c
+++ main.c
@@ -6,50 +6,25 @@ void help(){
     printf("\n cd - change directory");
     printf("\n nd - new directory");
     printf("\n ls - show directory");
-    printf("\n ls -R - recursive directory(beta)");
-    printf("\n help - show comands list");
+    printf("\n help - show commands list");
     printf("\n exit - terminate\n\n");
 }
 
 
-void RecDir(char *path, int flag) {
-    DIR *dp = opendir(path);
 
-
-    if(!dp) {
-        perror(path);
-        return;
-    }
-
-    struct dirent *ep;
-    char newdir[512];
-
-    printf("\n%s :\n" , path);
-
-    while((ep = readdir(dp)))
-        if(strncmp(ep->d_name, ".", 1))
-            printf( "\t%s\n", ep->d_name);
-
-    closedir(dp);
-    dp = opendir(path);
-
-    while((ep = readdir(dp))) if(strncmp(ep->d_name, ".", 1)) {
-            if(flag && ep-> d_type == 4) {
-                sprintf(newdir, "%s/%s", path, ep->d_name);
-                RecDir(newdir, 1);
-            }
-        }
-
-    closedir(dp);
-}
 
 void PrintDir(char *cdir){
 
+    printf("firststate\n");
 
     DIR *dp = opendir(cdir);
-
+    printf("2\n");
+    printf(cdir);
+    printf("1\n");
     struct dirent *ep;
-
+    if (dp == NULL){
+        printf("Null");
+    }
     if (dp != NULL) {
         while ((ep = readdir(dp))) {
             if (ep->d_name[0] != '.')
@@ -64,28 +39,42 @@ void PrintDir(char *cdir){
 }
 
 int main(int argc, char *argv[]) {
-    char cd[100] = "";
-    char temps[30] = "";
-    char arg[30] = "";
+
+    char *tempcd;
+    char *temps;
+    char temp[30] = "123";
+    char *arg;
     bool EM = false;
 
+    gets(temp);
+    printf(temp);
+
+    temps = temp;
+    printf(temps);
+    argv[2] = temps;
+    tempcd = argv[2];
 
     while (!EM) {
-        gets(arg);
-        if (strcmp(arg, "cd") == 0) {
-            gets(temps);
-            strcat(cd, temps);
-            printf(cd);
+        argv[1] = gets(temp);
+        //gets(arg);
+        if (strcmp(argv[1], "cd") == 0) {
+            printf("1123\n");
+            argv[2] = gets(temp);
+            printf(argv[2]);
             printf("\n");
-        } else if (strcmp(arg, "ls") == 0) {
-            PrintDir(cd);
-        } else if (strcmp(arg, "ls -R") == 0) {
-            RecDir(cd, 1);
-        } else if (strcmp(arg, "exit") == 0) {
+        } else if (strcmp(argv[1], "ls") == 0) {
+            printf("321\n");
+
+            printf(tempcd);
+
+            PrintDir(tempcd);
+        } else if (strcmp(argv[1], "exit") == 0) {
             EM = true;
-        } else if (strcmp(arg, "nd") == 0) {
-            gets(cd);
-        } else if (strcmp(arg, "help") == 0) {
+        } else if (strcmp(argv[1], "nd") == 0) {
+            argv[2] = gets(temp);
+            printf(argv[2]);
+            printf("/n");
+        } else if (strcmp(argv[1], "help") == 0) {
             help();
         } else {
             printf("try again\n");
@@ -93,5 +82,5 @@ int main(int argc, char *argv[]) {
     }
 
 
-    return 0;
+    return -1;
 }
